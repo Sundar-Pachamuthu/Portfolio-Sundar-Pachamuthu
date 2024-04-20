f3720f7e94f7355b116b3516eaa8ab9c3e6f03d5
Sundar-Pachamuthu <sundarkarthick574@gmail.com>
2024-04-20 17:26:32 +0530

    first upload

---
+++ Main.js
@@ -0,0 +1,37 @@
+let menuIcon = document.getElementById('menu-icon');
+let navbar = document.getElementById('nav');
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle('bi-x');
+    navbar.classList.toggle('flex')
+}
+
+window.onscroll= ()=>{
+    let sections = document.querySelectorAll('section');
+    let navLinks =  document.querySelectorAll('Header nav a');
+
+
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 300;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+  
+        if(top >= offset && top < offset + height){
+            // active nav links
+            navLinks.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('Header   nav a[href*=' + id + ']').classList.add('active');
+            });
+            sec.classList.add("show-animate");
+        }
+        else{
+            sec.classList.remove("show-animate");
+
+        }
+    })
+
+    let header= document.querySelector('Header');
+    header.classList.toggle('sticky', window.scrollY > 70);
+}
+
+
